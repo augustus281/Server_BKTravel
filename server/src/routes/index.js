48ed99352ef82e74e65036ef7c4055f6fa09accc
I am Augustus <theflash28012002@gmail.com>
2024-03-28 23:40:44 +0700

    add route log discord

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,9 +2,12 @@
 
 const express = require('express')
 const router = express.Router()
+const { pushToLogDiscord } = require("../middlewares/index")
 
+router.use(pushToLogDiscord)
 router.use('/api/v1/auth', require("./auth"))
 router.use('/api/v1/attraction', require("./attraction"))
+router.use('/api/v1/comment', require("./comment"))
 router.use('/api/v1/destination', require("./destination"))
 router.use('/api/v1/schedule', require("./schedule"))
 router.use('/api/v1/user', require("./user"))
