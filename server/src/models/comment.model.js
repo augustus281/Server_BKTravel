249a0a404be7b315a589e65e83b11927b81642ab
I am Augustus <theflash28012002@gmail.com>
2024-03-28 23:38:54 +0700

    comment model

---
+++ server/src/models/comment.model.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const { Model, DataTypes } = require("sequelize")
+const sequelize = require("../database/index")
+
+class Comment extends Model {}
+Comment.init({
+    comment_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    comment_left: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    },
+    comment_right: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    },
+    parent_comment_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: Comment,
+            key: "comment_id"
+        }
+    },
+    is_deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+}, { sequelize, modelName: "comment" })
+
+Comment.hasMany(Comment, { as: 'children', foreignKey: 'parent_comment_id' });
+Comment.belongsTo(Comment, { as: 'parent', foreignKey: 'parent_comment_id' });
+
+module.exports = Comment
