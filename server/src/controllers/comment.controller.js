b4d5ebed64d85c9fe52ff6cc0a8b6f4ce87d912d
I am Augustus <theflash28012002@gmail.com>
2024-03-28 23:39:24 +0700

    creata comment: nested comment

---
+++ server/src/controllers/comment.controller.js
@@ -0,0 +1,112 @@
+'use strict'
+
+const { Sequelize } = require("sequelize")
+const Comment = require("../models/comment.model")
+
+const Op = Sequelize.Op
+
+class CommentController {
+    createComment = async (req, res, next) => {
+    try {
+        const { tour_id, content, user_id, parent_comment_id } = req.body;
+        const comment = await Comment.create({
+            tour_id, content, user_id, parent_comment_id
+        });
+
+        let rightValue;
+        if (parent_comment_id) {
+            // reply comment
+            const parent_comment = await Comment.findOne({ where: { comment_id: parent_comment_id }});
+            if (!parent_comment) return res.status(404).json({ message: "Not found parent comment!" });
+
+            console.log(`parent_comment:::`, parent_comment)
+
+            rightValue = parent_comment.comment_right;
+
+            // update many comments
+            await Comment.update({ 
+                comment_right: Sequelize.literal('comment_right + 2')
+            },{ 
+                where: { 
+                    tour_id: tour_id,
+                    comment_right: { [Sequelize.Op.gte]: rightValue }
+                }
+            });
+
+            await Comment.update({ 
+                    comment_left: Sequelize.literal('comment_left + 2')
+                },{ 
+                    where: { 
+                        tour_id: tour_id,
+                        comment_left: { [Sequelize.Op.gt]: rightValue }
+                    }
+            });
+        } else {
+            const maxRightValue = await Comment.findOne({
+                where: {
+                    tour_id: tour_id
+                },
+                attributes: [[Sequelize.fn('MAX', Sequelize.col('comment_right')), 'maxRight']]
+            });
+
+            rightValue = maxRightValue && maxRightValue.dataValues.maxRight !== null ? maxRightValue.dataValues.maxRight + 1 : 1;
+        }
+
+        // insert to comment
+        comment.comment_left = rightValue;
+        comment.comment_right = rightValue + 1;
+
+        await comment.save();
+        return res.status(200).json({
+            message: "You comment tour successfully!",
+            comment: comment
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+
+    getCommentsByParentId = async (req, res, next) => {
+        try {
+            const { tour_id, parent_comment_id, limit = 50, offset = 0 } = req.body;
+            let comments;
+            
+            if (parent_comment_id) {
+                const parent = await Comment.findOne({ where: { parent_comment_id: parent_comment_id }});
+                if (!parent) return res.status(404).json({ message: "Not found comment for tour!"});
+
+                comments = await Comment.findAll({
+                    where: {
+                        tour_id: tour_id,
+                        comment_left: { [Op.gt]: parent.comment_left },
+                        comment_right: { [Op.lte]: parent.comment_right }
+                    },
+                    attributes: ['comment_left', 'comment_right', 'content', 'parent_comment_id'],
+                    order: [['comment_left', 'ASC']],
+                    limit,
+                    offset
+                });
+            } else {
+                comments = await Comment.findAll({
+                    where: {
+                        tour_id: tour_id,
+                        parent_comment_id: parent_comment_id
+                    }, attributes: ['comment_left', 'comment_right', 'content', 'parent_comment_id'],
+                    order: [['comment_left', 'ASC']],
+                    limit,
+                    offset
+                });
+            }
+
+            return res.status(200).json({
+                message: "Get all comment by parent_comment_id successfully",
+                comments: comments
+            })
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
+    }
+}
+
+module.exports = new CommentController()
