92e03fc4b7e1d2711bcab856b74e1e70f4347f54
I am Augustus <theflash28012002@gmail.com>
2024-04-01 21:33:12 +0700

    createrOrderFromCart route

--- server/src/routes/order/index.js
+++ server/src/routes/order/index.js
@@ -6,6 +6,7 @@ const { asyncHandler } = require('../../auth/checkAuth')
 const  orderController  = require("../../controllers/order.controller")
 
 router.post("/", asyncHandler(orderController.createOrderByTourId))
+router.post("/create-from-cart", asyncHandler(orderController.createOrderFromCart))
 router.post("/payment", asyncHandler(orderController.payOrderDirectly))
 router.post("/:order_id/applyVoucher", asyncHandler(orderController.applyVoucherToOrder))
 
