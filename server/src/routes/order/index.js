ee2a2f298a8c8a25a3c8d29a8a7e91fc7786bd20
I am Augustus <theflash28012002@gmail.com>
2024-04-01 22:49:19 +0700

    pending & complete order route

--- server/src/routes/order/index.js
+++ server/src/routes/order/index.js
@@ -8,6 +8,8 @@ const  orderController  = require("../../controllers/order.controller")
 router.post("/", asyncHandler(orderController.createOrderByTourId))
 router.post("/create-from-cart", asyncHandler(orderController.createOrderFromCart))
 router.post("/payment", asyncHandler(orderController.payOrderDirectly))
+router.get("/:user_id/pending", asyncHandler(orderController.getPendingOrderByUser))
+router.get("/:user_id/complete", asyncHandler(orderController.getCompleteOrderByUser))
 router.post("/:order_id/applyVoucher", asyncHandler(orderController.applyVoucherToOrder))
 
 module.exports = router
