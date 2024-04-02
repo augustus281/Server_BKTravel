029b468504c65adaa04b8ac0b991a61eb6ddd744
I am Augustus <theflash28012002@gmail.com>
2024-04-02 14:48:11 +0700

    fix format

--- server/src/routes/comment/index.js
+++ server/src/routes/comment/index.js
@@ -4,8 +4,9 @@ const express = require("express")
 const router = express.Router()
 const { asyncHandler } = require('../../auth/checkAuth')
 const commentController = require("../../controllers/comment.controller")
+const formidableMiddleware = require('express-formidable');
 
-router.post("/", asyncHandler(commentController.createComment))
+router.post("/", formidableMiddleware(), asyncHandler(commentController.createComment))
 router.get("/", asyncHandler(commentController.getCommentsByParentId))
 router.delete("/", asyncHandler(commentController.deleteComment))
 
