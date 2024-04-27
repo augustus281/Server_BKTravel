98459ceac081606b77fb5f7ed5e82c2c2862e0ce
DucHuy2801 <augustus@nautilusgames.com>
2024-04-28 02:48:13 +0700

    rename

--- server/src/config/config.js
+++
@@ -1,8 +0,0 @@
-// const config = {
-//     redis: {
-//         port: process.env.REDIS_PORT,
-//         host: process.env.REDIS_HOST
-//     }
-// }
-
-// module.exports = config
--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -61,3 +61,4 @@ io.on("connection", (socket) => {
     });
 
 })
+
