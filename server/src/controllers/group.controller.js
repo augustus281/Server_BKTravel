d3593df982818382b8538bed838cee6d03e95773
DucHuy2801 <augustus@nautilusgames.com>
2024-05-04 00:22:36 +0700

    add joinGroup controller

--- server/src/controllers/group.controller.js
+++ server/src/controllers/group.controller.js
@@ -1,11 +1,14 @@
 'use strict'
 
+const { BadRequestError } = require("../core/error.response")
 const Group = require("../models/group.model")
 const GroupUser = require("../models/group_member.model")
 const Message = require("../models/message.model")
+const User = require("../models/user.model")
 const { findGroupByID } = require("../services/group.service")
+const { checkOrderTour, checkCompleteOrder } = require("../services/order.service")
 const { findTourById } = require("../services/tour.service")
-const { findUserById } = require("../services/user.service")
+const { findUserById, checkCartByUser } = require("../services/user.service")
 
 class GroupController {
     createGroup = async (req, res, next) => {
@@ -97,6 +100,45 @@ class GroupController {
                 group_id: group_id,
                 user_id: user_id
             })
+
+            group.number++;
+            await group.save();
+    
+            if (!joined) return res.status(400).json({ message: "Failed to join group" })
+            return res.status(200).json({ message: "Add user to group chat successfully!" })
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
+    }
+
+    joinGroup = async (req, res, next) => {
+        try {
+            const group_id = req.params.group_id
+            const { user_id, tour_id, order_id } = req.body
+
+            console.log(req.body)
+    
+            const user = await findUserById(user_id)
+            if (!user) return res.status(404).json({ message: "Not found user" })
+
+            // check order tour
+            const orderTour = await checkOrderTour(order_id, tour_id);
+            if (!orderTour) throw new BadRequestError("Can't join group")
+
+            // check order is completed
+            const completeOrder = await checkCompleteOrder(order_id, user_id);
+            if (!completeOrder) throw new BadRequestError("Order is not completed to join group chat!")
+            
+            const group = await findGroupByID(group_id)
+            if (!group) return res.status(404).json({ message: "Not found group to join" })
+    
+            const joined = await GroupUser.create({
+                group_id: group_id,
+                user_id: user_id
+            })
+
+            group.number++;
+            await group.save();
     
             if (!joined) return res.status(400).json({ message: "Failed to join group" })
             return res.status(200).json({ message: "Join group chat successfully!" })
