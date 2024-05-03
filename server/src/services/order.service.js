468e1784a4e1d9e5e594d8f16198587a31b65801
DucHuy2801 <augustus@nautilusgames.com>
2024-05-04 00:23:47 +0700

    add checkOrderTour & checkCompleteOrder

--- server/src/services/order.service.js
+++ server/src/services/order.service.js
@@ -1,7 +1,9 @@
 'use strict'
 
+const { StatusOrder } = require("../common/status")
 const Order = require("../models/order.model")
 const OrderItem = require("../models/order_item.model")
+const OrderTour = require("../models/order_tour.model")
 
 const findOrderItem = async (cart_id, tour_id) => {
     return await OrderItem.findOne({
@@ -19,7 +21,33 @@ const findOrderById = async (order_id) => {
         }
     })
 }
+
+const checkOrderTour = async (order_id, tour_id) => {
+    const orderTour = await OrderTour.findOne({
+        where: {
+            tour_id: tour_id,
+            order_id: order_id
+        }
+    })
+
+    return orderTour
+}
+
+const checkCompleteOrder = async (order_id, user_id) => {
+    const isOrdered = await Order.findOne({
+        where: {
+            user_id: user_id,
+            order_id: order_id,
+            status: StatusOrder.COMPLETE
+        }
+    })
+
+    return isOrdered
+}
+
 module.exports = {
     findOrderItem,
-    findOrderById
+    findOrderById,
+    checkCompleteOrder,
+    checkOrderTour
 }
