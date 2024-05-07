6aaa420e27a563396d7e5041bb343e9ac09201d0
DucHuy2801 <augustus@nautilusgames.com>
2024-05-07 23:27:56 +0700

    fix updateTotalCart

--- server/src/services/cart.service.js
+++ server/src/services/cart.service.js
@@ -7,12 +7,10 @@ const updateTotalCart = async (user_id, total_price) => {
         where: { user_id: user_id }
     });
     cart.total = cart.total - parseFloat(total_price);
+    if (cart.total <= 0) cart.total = 0;
     await cart.save();
 }
 
-const updateTotalPriceOrderItem = async (item_id, ) => {
-
-}
 module.exports = {
     updateTotalCart
 }
