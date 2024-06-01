4d96813876b5db2f5e0af0be0dc19ff4fb586bc0
DucHuy2801 <theflash28012002@gmail.com>
2024-06-01 16:39:14 +0700

    init route: getRefundPayment & refundPayment

--- server/src/routes/payment/index.js
+++ server/src/routes/payment/index.js
@@ -8,6 +8,8 @@ const { authenticate } = require("../../middlewares/authenticate")
 
 router.post("/", authenticate, asyncHandler(paymentController.createPaymentUrl))
 router.post("/momo", asyncHandler(paymentController.paymentWithMomo))
+router.get("/refund", authenticate,  asyncHandler(paymentController.getRefundPayment))
+router.post("/refund", authenticate,  asyncHandler(paymentController.refundPayment))
 router.get("/vnpay_ipn", authenticate, asyncHandler(paymentController.getResultPayment))
 
 module.exports = router
