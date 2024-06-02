05b39f8dcd4932b2622d2137bbf74f5acc72321c
DucHuy2801 <theflash28012002@gmail.com>
2024-06-03 00:47:11 +0700

    Create schedule for user

--- server/src/controllers/schedule.controller.js
+++ server/src/controllers/schedule.controller.js
@@ -75,6 +75,64 @@ class ScheduleController {
         }
     }
 
+    createScheduleForUser = async(req, res, next) => {
+        try {
+            const tour_id = req.body.tour_id
+            const tour = await findTourById(tour_id)
+            if (!tour) return res.status(404).json({ message: "Not found tour for creating schedule!" })
+
+            const exist_schedule = await Schedule.findOne({ where: { tour_id: tour_id }})
+            if (exist_schedule) return res.status(400).json({ message: "Tour has already been scheduled! "})
+
+            const schedule_detail = req.body.schedule_detail
+
+            for (const schedule of schedule_detail) {
+                for (const detail of schedule.detail) {
+                    const name = detail.name;
+
+                    const attraction = await Attraction.findOne({ where: { name: name }})
+                    if (!attraction) {
+                        let exist_attraction = await OtherAttraction.findOne({ where: { name: name }})
+                        if (!exist_attraction) {
+                            exist_attraction = await OtherAttraction.create({
+                                name: detail.name,
+                                note: detail.note || null,
+                                description: detail.description
+                            })
+                        }
+                        else {
+                            exist_attraction.note = detail.note || null;
+                            exist_attraction.description = detail.description;
+                            await exist_attraction.save()
+                        }
+                    }
+                    else {
+                        attraction.note = detail.note || null;
+                        attraction.description = detail.description;
+                        await attraction.save()
+                    }
+                }
+            }
+            const new_schedule = await Schedule.create({
+                schedule_detail: JSON.parse(JSON.stringify(schedule_detail)),
+                tour_id: tour_id
+            })
+
+            await tour.save()
+
+            redisClient.del("waiting_tours")
+            redisClient.del("online_tours")
+            
+            return res.status(201).json({ 
+                data: new_schedule,
+                message: "Create schedule for user's tour successfully! "
+            })
+            
+        } catch(error) {
+            return res.status(500).json({ message: error.message })
+        }
+    }
+
     updateSchedule = async (req, res, next) => {
         try {
             const id = req.params.id;
--- server/src/routes/schedule/index.js
+++ server/src/routes/schedule/index.js
@@ -7,6 +7,7 @@ const scheduleController = require("../../controllers/schedule.controller")
 const { authenticate, authenticateAdmin } = require("../../middlewares/authenticate")
 
 router.post("/", authenticate, asyncHandler(scheduleController.createSchedule))
+router.post("/users", authenticate, asyncHandler(scheduleController.createScheduleForUser))
 router.put("/", authenticate, asyncHandler(scheduleController.updateScheduleDetails))
 router.put("/:id", authenticateAdmin, asyncHandler(scheduleController.updateSchedule))
 router.get("/weather", asyncHandler(scheduleController.getWeatherData))
